1797d60adb3384dd5216ccb0ec2a5f698c4e10b0
Flapjacck <plkelly333@me.com>
2024-10-10 13:12:51 -0400

    Finished l06, queue_insert gives error: signal code [6]: Aborted

--- Kell6733_l06/src/queue_linked.c
+++ Kell6733_l06/src/queue_linked.c
@@ -65,18 +65,68 @@ int queue_count(const queue_linked *source) {
 void queue_insert(queue_linked *source, data_ptr item) {
 
 	// your code here
+	// Allocate memory
+	queue_node *new_node = (queue_node*) malloc(sizeof(queue_node));
 
+	// Copy the item into the new node
+	new_node->item = item;
+	new_node->next = NULL;
+
+	// If the queue is empty, new node becomes both the front and rear
+	if (source->rear == NULL) {
+		source->front = new_node;
+		source->rear = new_node;
+	} else {
+		// Attach the new node to the end of the queue and update the rear
+		source->rear->next = new_node;
+		source->rear = new_node;
+	}
+
+	// Increase the count
+	source->count++;
 }
 
 BOOLEAN queue_peek(const queue_linked *source, data_ptr item) {
 
 	// your code here
+	// Check if the queue is empty
+	if (queue_empty(source)) {
+		return FALSE;
+	}
+	// Copy the data
+	data_copy(item, source->front->item);
+	return TRUE;
 
 }
 
 BOOLEAN queue_remove(queue_linked *source, data_ptr *item) {
 
 	// your code here
+	// Check if the queue is empty
+	if (source->front == NULL) {
+		return FALSE; // Queue is empty
+	}
+
+	// Get the front node and its item
+	queue_node *temp = source->front;
+	*item = temp->item;
+
+	// Move the front pointer to the next node
+	source->front = source->front->next;
+
+	// If the front becomes NULL, then the queue is empty, update rear to NULL
+	if (source->front == NULL) {
+		source->rear = NULL;
+	}
+
+	// Free the memory of the removed node
+	free(temp);
+
+	// Decrease the count of items in the queue
+	source->count--;
+
+	// Return TRUE, the item has been removed
+	return TRUE;
 
 }
 
